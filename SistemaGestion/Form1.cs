202f88e7eeef901b3a99a46a05db725af8af510e
Roberto Ferreira <fpontes23@gmail.com>
2016-06-18 08:41:44 -0400

    Prueba de commit

--- SistemaGestion/Form1.cs
+++ SistemaGestion/Form1.cs
@@ -16,5 +16,10 @@ namespace SistemaGestion
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string strPrueba = "";
+        }
     }
 }
